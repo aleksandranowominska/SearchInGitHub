2854456419f9d4fe3cd09f67824771f08db51748
Olka Kusiak <olkakusiak@gmail.com>
2016-11-26 19:30:40 +0100

    searching typed data in API (without displaying)

--- SearchInGitHub/SearchViewController.swift
+++ SearchInGitHub/SearchViewController.swift
@@ -8,26 +8,24 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
 
 	@IBOutlet weak var tableView: UITableView!
-	
+	@IBOutlet weak var searchInGitHub: UISearchBar!
+	var resultSearchController = UISearchController()
+	var userArray: [AnyObject] = []
+	var repoArray: [AnyObject] = []
+	var resultArray: [AnyObject] = []
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-		DataManager.instance.getUsers(query: "tom", userNamesDownloaded: {users in
-			print("found \(users.count) users")
-		}, userError: {error in
-		print("user error")
 		
-		})
-		DataManager.instance.getRepos(query: "tom", userReposDownloaded: {repos in
-			print("found \(repos.count) repos")
-		}, repoError: {error in
-			print("repo error")
-			})
-
-        // Do any additional setup after loading the view.
+		self.resultSearchController = UISearchController(searchResultsController: nil)
+		self.resultSearchController.searchResultsUpdater = self
+		self.resultSearchController.dimsBackgroundDuringPresentation = false
+		self.resultSearchController.searchBar.sizeToFit()
+		self.tableView.tableHeaderView = self.resultSearchController.searchBar
+		self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,14 +45,46 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
 		return UITableViewCell()
 	}
 	
-    /*
-    // MARK: - Navigation
+	//function which is launched when user is searching sth
+	func updateSearchResults(for searchController: UISearchController) {
+		let searchText = searchController.searchBar.text!
+		let progressCircle = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+		progressCircle.hidesWhenStopped = true
+		progressCircle.tag = 2
+		searchController.searchBar.addSubview(progressCircle)
+		progressCircle.startAnimating()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+		//search users
+		DataManager.instance.getUsers(query: searchText, userNamesDownloaded: {users in
+			print("found \(users.count) users")
+			self.userArray = users
+			progressCircle.stopAnimating()
+			self.refreshTableView()
+		}, userError: {error in
+			print("user error")
+			self.userArray = []
+			progressCircle.stopAnimating()
+			self.refreshTableView()
+		})
+		//search repos
+		DataManager.instance.getRepos(query: searchText, userReposDownloaded: {repos in
+			print("found \(repos.count) repos")
+			self.repoArray = repos
+			progressCircle.stopAnimating()
+			self.refreshTableView()
+		}, repoError: {error in
+			print("repo error")
+			self.repoArray = []
+			progressCircle.stopAnimating()
+			self.refreshTableView()
+		})
+		
+	}
+	
+	func refreshTableView(){
+		resultArray = userArray + repoArray
+		print("result array is \(resultArray.count)")
+		self.tableView.reloadData()
+	}
 
 }
