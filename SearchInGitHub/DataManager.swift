8ca0a083dd16575239b57af61b558cb582b4a1ed
Olka Kusiak <olkakusiak@gmail.com>
2016-11-26 16:05:37 +0100

    pods added: Alamofire, SwiftyJSON, created DataManager file

---
+++ SearchInGitHub/DataManager.swift
@@ -0,0 +1,21 @@
+//
+//  DataManager.swift
+//  SearchInGitHub
+//
+//  Created by Aleksandra Kusiak on 26.11.2016.
+//  Copyright © 2016 ola. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class DataManager{
+	
+	static let instance = DataManager()
+	
+	private init(){
+	}
+	
+	
+}
---
+++ SearchInGitHub/SearchViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SearchViewController.swift
+//  SearchInGitHub
+//
+//  Created by Aleksandra Kusiak on 26.11.2016.
+//  Copyright © 2016 ola. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+	@IBOutlet weak var tableView: UITableView!
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+	
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
